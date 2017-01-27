acfc00d347e13e2fe18a87975f5329f1b4af1f33
W-Mark Kubacki <wmark@hurrikane.de>
2017-01-27 12:55:16 +0100

    sync the bootstrapping script with upstream

--- kvm-bootstrap.sh
+++ kvm-bootstrap.sh
@@ -38,7 +38,7 @@ fi
 # example:
 #   windows ${COREOS_PUBLIC_IPV4} 5900 geheim 52:54:00:xx:xx:xx macvtap0 "windows-1" $((16 * 8 * 1024)) Windows-10-threshold-2-take-1.iso
 
-if (( $# >= 9 )) && [[ "$1" == "windows" ]]; then
+if (( $# >= 8 )) && [[ "$1" == "windows" ]]; then
   curl --silent --show-error --fail --location --remote-time \
     --{time-cond,output}/var/cache/media/virtio-win.iso \
     https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso \
@@ -55,19 +55,40 @@ if (( $# >= 9 )) && [[ "$1" == "windows" ]]; then
 
 #   -device virtio-balloon,id=balloon0,bus=pci.0,addr=0x7 \ Windows 10 won't start with this
 
+  : ${ncores:="4"}
+  if (( $(nproc) > 16 )); then
+    # MCC or HCC cpu(s)
+    if (( $(nproc) > 20)); then
+      let ncores="$[ $(nproc --ignore 4)/2 ]"
+    else
+      let ncores="$(nproc --ignore 2)"
+    fi
+  fi
+
+  drives=()
+  drives+=("-drive" "file=/var/vm/disks/$7.img,if=virtio,index=0,media=disk")
+  drives+=("-drive" "file=/var/cache/media/virtio-win.iso,index=3,media=cdrom,readonly")
+  if (( $# >= 9 )); then
+    if [[ -s "/var/cache/media/$9" ]]; then
+      drives+=("-drive" "file=/var/cache/media/$9,index=2,media=cdrom,readonly")
+      drives+=("-boot" "once=d")
+    else
+      >&2 printf "Ignored, because it is no file: %s\n" "$9"
+    fi
+  fi
+  spice=()
+  spice+=("-vga" "none" "-device qxl-vga,vgamem_mb=32")
+  spice+=("-spice" "addr=$2,port=$3,password=$4")
+  spice+=("-chardev" "spicevmc,id=vdagent,name=vdagent")
+  spice+=("-device" "virtserialport,chardev=vdagent,name=com.redhat.spice.0")
+
   exec /usr/bin/qemu -enable-kvm -nographic -rtc base=utc \
     -monitor unix:/run/kvm/"$7".monitor,server,nowait \
-    -cpu host -m $8 -smp 4,sockets=1 -k de -usbdevice tablet \
-    -vga none -device qxl-vga,vgamem_mb=32 \
-    -spice addr=$2,port=$3,password="$4" \
+    -cpu host -m $8 -smp ${ncores},sockets=1 -k de -usbdevice tablet \
     -device virtio-serial \
-    -chardev spicevmc,id=vdagent,name=vdagent \
-    -device virtserialport,chardev=vdagent,name=com.redhat.spice.0 \
+    ${spice[*]} \
     -net nic,model=virtio,macaddr=$5 -net tap,fd=3 3<>/dev/tap-vm \
-    -drive file=/var/vm/disks/"$7".img,if=virtio,index=0,media=disk \
-    -drive file=/var/cache/media/"$9",index=2,media=cdrom,readonly \
-    -drive file=/var/cache/media/virtio-win.iso,index=3,media=cdrom,readonly \
-    -boot once=d \
+    ${drives[*]} \
     -name "$7"
 else
   exec /usr/bin/qemu -enable-kvm "$@"
